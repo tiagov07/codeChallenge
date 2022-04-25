4b1a9c80bdd9e6b378c422fb1f284323f585bfb4
tiagov07 <santiago.valencia@mercadolibre.com.co>
2022-04-25 16:27:34 -0500

    working on validTime codeSignal challenge

---
+++ validTime.js
@@ -0,0 +1,26 @@
+// Check if the given string is a correct time representation of the 24-hour clock.
+
+// Example
+
+// For time = "13:58", the output should be
+// solution(time) = true;
+// For time = "25:51", the output should be
+// solution(time) = false;
+// For time = "02:76", the output should be
+// solution(time) = false.
+function solution(time) {
+    let parts = time.split(":")
+    if(parts[0] < 24 && parts[1] < 60) return true
+    else return false
+}
+
+//go solution
+import "strings"
+
+// func solution(time string) bool {
+//      parts := strings.Split(time, ":")
+//      hours,_ := strconv.Atoi(parts[0])
+//      minutes,_ := strconv.Atoi(parts[1])
+//      if hours < 24 && minutes < 60 {return true} 
+//      return false
+// }
