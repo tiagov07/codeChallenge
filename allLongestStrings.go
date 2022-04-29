d95d0f88bd20e86d2affb6a6b1b6d8a4146a7712
tiagov07 <santiago.valencia@mercadolibre.com.co>
2022-04-28 20:00:57 -0500

    working on allLongestString codeSIgnal challenge

---
+++ allLongestStrings.go
@@ -0,0 +1,26 @@
+// Given an array of strings, return another array containing all of its longest strings.
+
+// Example
+
+// For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+// solution(inputArray) = ["aba", "vcd", "aba"].
+package main
+
+func solution(inputArray []string) []string {
+	longestWord := 0
+	solution := make([]string, 0)
+
+	for _, el := range inputArray {
+		if len(el) == longestWord {
+			solution = append(solution, el)
+		}
+		if len(el) > longestWord {
+			longestWord = len(el)
+			solution = []string{el}
+
+		}
+	}
+	return solution
+}
+
+//10
